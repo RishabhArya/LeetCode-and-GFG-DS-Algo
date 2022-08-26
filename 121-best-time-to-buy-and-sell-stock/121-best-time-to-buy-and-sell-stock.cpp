bffb48eef35072481a833c801292d3b5971a3d8d
Rishabh Arya <rishabharya2799@gmail.com>
2022-08-27 01:30:45 +0530

    Time: 173 ms (70.94%), Space: 93.4 MB (52.40%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -4,8 +4,8 @@ public:
         int minVal = prices[0];
         int maxProfit = 0;
         for(int i=1;i<prices.size();i++){
-            if(prices[i]<minVal)
-                minVal = prices[i];
+            if(prices[i]<minVal){
+                minVal = prices[i];continue;}
             int currentProfit = prices[i] - minVal;
             if(currentProfit > maxProfit)
                 maxProfit = currentProfit;
