39f226b036de4ed39676208206cdbc79a870d026
Rishabh Arya <rishabharya2799@gmail.com>
2022-09-22 20:01:27 +0530

    Time: 13 ms (5.42%), Space: 5.9 MB (81.51%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -3,6 +3,7 @@ public:
     double myPow(double x, int n) {
          if(n==0) return 1;
         double t = myPow(x,n/2);
+        cout<<t<<" ";
         if(n%2) return n<0 ? 1/x*t*t : x*t*t;
         else return t*t;
     }
